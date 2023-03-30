e5c7f089bb5ea3b2798c5a580930574a5b9c9580
gregory hendrickson <greg@hndrx.co>
2023-03-30 04:51:23 -0700

    fix broken line

--- main.tf
+++ main.tf
@@ -13,8 +13,6 @@ resource "google_project_service" "compute_engine_api" {
   disable_on_destroy         = false
 }
 
-  disable_dependent_services = true
-}
 module "compute_engine_template" {
   source = "./modules/compute_engine_template"
   project_id  = var.project_id
